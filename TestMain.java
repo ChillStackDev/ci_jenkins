869daaa915c583645f9021d60d6ffbb1bc3f0075
ChillStackDev <144476531+ChillStackDev@users.noreply.github.com>
2024-08-02 16:36:37 +0530

    Update TestMain.java

--- TestMain.java
+++ TestMain.java
@@ -1,5 +1,6 @@
 public class TestMain{
     public static void main(String[] args){
 		  System.out.println("Welcome to the world of jenkins");
+		  System.out.println("Jenkins Poll SCM worked!");
 	}
 }
